6c5b1884388489e4e76917afe92f50455517f3de
Narendra <narendra.punchh.com>
2020-10-21 18:07:18 +0530

    Bug fix

--- App/Components/custombutton.js
+++ App/Components/custombutton.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  ImagePropTypes,
-} from 'react-native';
-const custombutton =(props)=>{
-    return(
-         <Button title="KHLKKJL"
-        onPress={()=> props.onPress()}
-        /> 
-    );
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, ImagePropTypes } from 'react-native';
 
-}
+const custombutton = (props) => {
+	return <Button title="KHLKKJL" onPress={() => props.onPress()} />;
+};
+
 export default custombutton;
